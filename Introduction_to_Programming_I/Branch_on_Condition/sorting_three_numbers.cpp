7642c99a427e1b2c4286cb5bf3c34a0f30b3a8c0
ssato <ssato@sf.ecei.tohoku.ac.jp>
2018-12-22 01:32:27 +0900

    first commit

---
+++ Introduction_to_Programming_I/Branch_on_Condition/sorting_three_numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <utility>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  if (a > b) swap(a, b);
+  if (b > c) swap(b, c);
+  if (a > b) swap(a, b);
+
+  printf("%d %d %d\n", a, b, c);
+  return 0;
+}
